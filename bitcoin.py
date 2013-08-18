9d01e71976ef0e3b1a78ce98347e32743b06447f
CJ Holmes <slashingweapon@gmail.com>
2013-08-18 09:05:27 -0700

    Turn the bitcoin object into a generally-useful json-rpc proxy

--- bitcoin.py
+++ bitcoin.py
@@ -1,22 +1,35 @@
+import urllib
+import uuid
+import json
 from django.conf import settings
-from jsonrpc.proxy import ServiceProxy
 
 class Bitcoin(object):
+    """ This is a proxy object for a json-rpc service.  I initially borrowed from 
+        django-json-rpc, but eventually departed to create my own proxy.
     
-    def __init__(self, url, method=None):
+        Once initialized, you can call any function freely.  Errors raise exceptions.  Other
+        kinds of responses are returned unmolested.  The overhead fields are thrown away.
+    """
+    def __init__(self, url):
         self._url = url
-        self._proxy = None
-        self._method = method
         
     def __getattr__(self, name):
-        return Bitcoin(self._url, name)
-        
-    def __call__(self, *args):
-        if (self._proxy == None):
-            self._proxy = ServiceProxy(settings.BITCOIN_URL)
-        response = self._proxy.__getattr__(self._method)(*args)
-        return self.parseResponse(response)
-            
+        """ Attributes are all functions.  Build a function with the right closure, so it
+            can be called with the desired parameters.
+        """
+        def callfunc(*args, **kwargs):
+            jsoncall = {
+                'jsonrpc':"2.0",
+                'id': str(uuid.uuid1()),
+                'method': name,
+                'params': kwargs if len(kwargs) else args
+            }
+            response = urllib.urlopen(self._url, json.dumps(jsoncall)).read()
+            response = json.loads(response)
+            return self.parseResponse(response)
+
+        return callfunc
+                    
     def parseResponse(self, response):
         if response.get(u'error') != None:
             code = response[u'error'].get(u'code', 'no-code')
